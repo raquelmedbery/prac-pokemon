43a3637ead9fc11d59f421e06d39942f23670644
raquelmedbery <50762368+raquelmedbery@users.noreply.github.com>
2019-10-03 13:09:08 -0500

    added two scripts for animation
    imported FBX animation and added the pokeTouch and animTouch script to make an animation appear when the pokemon is touched

---
+++ PokemonARPractice/Assets/animTouch.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animTouch : MonoBehaviour
+{
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //gameObject.SetActive(false);
+        //gameObject.GetComponent<Renderer>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        for (int i = 0; i < Input.touchCount; ++i)
+        {
+            if (Input.GetTouch(i).phase == TouchPhase.Began && anim.GetCurrentAnimatorStateInfo(0).IsName("none") && gameObject.activeSelf)
+            {
+                anim.Play("Fullanimation");
+                //gameObject.SetActive(true);
+            }
+            else if (Input.GetTouch(i).phase == TouchPhase.Began && anim.GetCurrentAnimatorStateInfo(0).IsName("Fullanimation") && gameObject.activeSelf)
+            {
+                anim.Play("none");
+            }
+        }
+    }
+}
---
+++ PokemonARPractice/Assets/pokeTouch.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pokeTouch : MonoBehaviour
+{
+    public GameObject bars;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //lightning.SetActive(false);
+        bars = GameObject.Find("shooting_bars");
+        bars.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (!(bars.activeSelf))
+        {
+            print("Bars should NOT be appearing");
+        }
+
+        for (int i = 0; i < Input.touchCount; ++i)
+        {
+
+            if (Input.GetTouch(i).phase == TouchPhase.Began && !(bars.activeSelf))
+            {
+
+                bars.SetActive(true);
+            }
+
+        }
+    }
+}
